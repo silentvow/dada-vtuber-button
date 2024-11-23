74cbe7aa6e8648f1129d5a776476e782ce629387
Peter Lin <silentvow@gmail.com>
2024-11-23 22:19:13 +0800

    fix: feedback margin

--- pages/feedback.vue
+++ pages/feedback.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-layout column justify-center align-center app>
-    <v-card class="readme-card">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSfhrSmBR5MLiHNn0uXgJI43Muavtjan3SeNBB_NFMu0u-hxeg/viewform?embedded=true"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        style="width: 100%; height: 100%; border: none"
-      >
-        載入中…
-      </iframe>
-    </v-card>
+  <v-layout column justify-center align-stretch app>
+    <v-flex xs12 sm8 md6>
+      <v-card class="readme-card">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfhrSmBR5MLiHNn0uXgJI43Muavtjan3SeNBB_NFMu0u-hxeg/viewform?embedded=true"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          style="width: 100%; height: 100%; border: none"
+        >
+          載入中…
+        </iframe>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 <script>
@@ -23,9 +25,12 @@ export default {
 };
 </script>
 <style lang="css">
+.v-card {
+  margin: 8px auto;
+}
+
 .readme-card {
   width: 100%;
   height: calc(100vh - 160px);
-  margin-bottom: -150px;
 }
 </style>
