ef433c1acf626eda8105b0af1541108d1256b0b1
Peter Lin <silentvow@gmail.com>
2024-11-23 20:59:43 +0800

    feat: update banner

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,10 @@ export default {
       { name: 'twitter:site', content: '@tn604000' }
     ],
     link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png' },
+      { rel: 'manifest', href: 'site.webmanifest' },
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
       { rel: 'shortcut icon', type: 'image/x-icon', href: 'favicon.ico' }
     ]
