e59a7ad697af4d017d4935b5aefa9fd46c6732d6
Peter Lin <silentvow@gmail.com>
2024-12-04 18:04:54 +0800

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier', 'plugin:prettier/recommended'],
+  // 校验 .vue 文件
+  plugins: ['vue', 'prettier'],
+  // 自定义规则
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'prettier/prettier': ['error']
+  }
+};
